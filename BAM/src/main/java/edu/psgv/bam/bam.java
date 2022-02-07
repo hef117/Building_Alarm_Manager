9be31713d332001c39a04e8be5bbbcd5b2d0b34c
basf <basf@DESKTOP-9MHB279.mshome.net>
2022-02-06 22:45:09 -0600

    First Commit

---
+++ BAM/src/main/java/edu/psgv/bam/bam.java
@@ -0,0 +1,17 @@
+package edu.psgv.bam;
+
+public class bam {
+
+	public static void main(String[] args) {
+		// this a test for the building alarm manager message
+		
+	String greeting = "alarm active"; 
+			int greetingLen= greeting.length();
+			for (int i=0; i<greetingLen; i++) {
+	           for (int j=0; j<i; j++) { System.out.print(" ");
+
+	}
+ System.out.println(greeting.charAt(i));
+}
+}
+}
